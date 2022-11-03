f84d136a6684fe4175494f049ea3adcd815144d5
Mir Faiyaz Hossain <63485441+mir8077faiyaz@users.noreply.github.com>
2022-11-04 01:14:46 +0600

    camera working + image saved tw60

--- rforce/models.py
+++ rforce/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserInfo(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    img= models.ImageField(unique=True,null=False)
+    
--- rforce/views.py
+++ rforce/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import sys
 import cv2
-
+from .models import UserInfo
 # Create your views here.
 def login(request):
     return render(request,'index.html')
@@ -11,5 +11,11 @@ def home(request):
     return render(request,'home.html')
 
 def setUpProfile(request):
+    if request.method=="POST":
+        user=UserInfo.objects.create(
+        img=request.POST.get("img_data"),
+        )
     return render(request,'setUpProfile.html')
+
+
 
