6cd96a89b852b82dc62e844395329133c65e26ad
Mir Faiyaz Hossain <63485441+mir8077faiyaz@users.noreply.github.com>
2022-11-04 14:41:44 +0600

    Face-Recognition 80%

--- rforce/models.py
+++ rforce/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    img= models.ImageField(unique=True,null=False)
+    img= models.TextField(unique=True,null=False)
     
--- rforce/views.py
+++ rforce/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 import sys
 import cv2
 from .models import UserInfo
+import face_recognition
+import base64
+import os
 # Create your views here.
 def login(request):
     return render(request,'index.html')
@@ -16,7 +19,31 @@ def setUpProfile(request):
         img=request.POST.get("img_data"),
         user = request.user
         )
+        # implementing fcr
+        db = UserInfo.objects.get(user=request.user)
+        db_img=db.img
+        dbu=db.user
+        #print(dbu)
+
+        with open(f"{dbu}.jpg", "wb") as fh:
+            fh.write(base64.b64decode(db_img))
+        #print(db_img)
+
+        known_image = face_recognition.load_image_file(f"{dbu}.jpg")
+        unknown_image = face_recognition.load_image_file("mir3.jpg")
+
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        print(results)
+        f = open(f"{dbu}.jpg", 'w')
+        f.close()
+        os.remove(f.name)
     return render(request,'setUpProfile.html')
+
+
+
 
 
 
